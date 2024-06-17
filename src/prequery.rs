e2ad51d836adcff4a749419b98180d9f5250003f
Clemens Koza <clemens.koza@gmx.at>
2024-06-17 22:44:12 +0200

    add resolve function (from Typst) as a utility for prequeries that want to save data

--- src/prequery.rs
+++ src/prequery.rs
@@ -1,3 +1,5 @@
+use std::path::{Component, Path, PathBuf};
+
 use anyhow::Result;
 use serde::Deserialize;
 
@@ -18,4 +20,29 @@ pub trait Prequery {
         let data = query::query(args, &config)?;
         Ok(data)
     }
+}
+
+/// Resolve the virtual path relative to an actual file system root
+/// (where the project or package resides).
+///
+/// Returns `None` if the path lexically escapes the root. The path might
+/// still escape through symlinks.
+pub fn resolve(path: &Path, root: &Path) -> Option<PathBuf> {
+    let root_len = root.as_os_str().len();
+    let mut out = root.to_path_buf();
+    for component in path.components() {
+        match component {
+            Component::Prefix(_) => {}
+            Component::RootDir => {}
+            Component::CurDir => {}
+            Component::ParentDir => {
+                out.pop();
+                if out.as_os_str().len() < root_len {
+                    return None;
+                }
+            }
+            Component::Normal(_) => out.push(component),
+        }
+    }
+    Some(out)
 }
