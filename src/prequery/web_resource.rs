4017b385c48fc41877500d4fe6fba7492d670323
Clemens Koza <clemens.koza@gmx.at>
2024-07-05 18:41:11 +0200

    add import

--- src/prequery/web_resource.rs
+++ src/prequery/web_resource.rs
@@ -3,6 +3,7 @@
 use anyhow::{anyhow, Result};
 use serde::Deserialize;
 
+use crate::args::CliArguments;
 use crate::config;
 use crate::query::Query;
 
@@ -31,7 +32,7 @@ impl PrequeryImpl for WebResource {
     type QueryData = Vec<Resource>;
 
     fn build_query(&self, config: config::Query) -> Result<Query> {
-        let config = Query::builder()        
+        let config = Query::builder()
             .default_field(Some("value".to_string()))
             .default_one(false)
             .default_selector("<web-resource>".to_string())
@@ -45,7 +46,7 @@ impl PrequeryImpl for WebResource {
 }
 
 impl Prequery for WebResource {
-    fn execute(&self, args: &crate::args::CliArguments, config: config::Query) -> Result<()> {
+    fn execute(&self, args: &CliArguments, config: config::Query) -> Result<()> {
         let result = self.query(&args, config)?;
         println!("{result:?}");
         Ok(())
