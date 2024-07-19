18b97879fd5a97dc04daed8f2cba2552e102d282
Clemens Koza <clemens.koza@gmx.at>
2024-07-19 10:33:01 +0200

    change module structure: move web_resource code to web_resource::factory, web_resource::preprocessor code to web_resource

--- src/preprocessors/web_resource.rs
+++ src/preprocessors/web_resource.rs
@@ -1,63 +1,222 @@
 //! The `web-resource` preprocessor
 
+use std::path::Path;
 use std::sync::Arc;
 
 use anyhow::{anyhow, Context, Result};
+use async_trait::async_trait;
+use tokio::fs;
+use tokio::io::AsyncWriteExt;
+use tokio::sync::Mutex;
+use tokio::task::JoinSet;
 
-use crate::config::Query as ConfigQuery;
-use crate::preprocessor::{BoxedPreprocessor, PreprocessorDefinition};
+use crate::args::ARGS;
+use crate::preprocessor::Preprocessor;
 use crate::query::Query;
 
 mod config;
+mod factory;
 mod index;
-mod preprocessor;
 mod query_data;
 
 use config::*;
 use index::*;
 use query_data::*;
 
-pub use preprocessor::WebResource;
+pub use factory::WebResourceFactory;
 
-/// The `web-resource` preprocessor factory
-#[derive(Debug, Clone, Copy)]
-pub struct WebResourceFactory;
+/// The `web-resource` preprocessor
+#[derive(Debug)]
+pub struct WebResource {
+    name: String,
+    config: Config,
+    index: Option<Mutex<Index>>,
+    query: Query,
+}
 
-impl WebResourceFactory {
-    fn parse_config(config: toml::Table) -> Result<Config> {
-        let config = config
-            .try_into()
-            .context("invalid web-resource configuration")?;
-        Ok(config)
+/// The state of the file: if and how the existing file corresponds to the desired web resource.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum ResourceState {
+    /// No local file exists.
+    Missing,
+    /// A re-download is forced despite the file existing.
+    Forced,
+    /// The file seems to be up-to-date: the URL hasn't changed, or no index is kept.
+    Existing,
+    /// The file seems is not up-to-date: the URL has changed according to the index.
+    ChangedResource,
+}
+
+impl ResourceState {
+    pub fn download(self) -> bool {
+        match self {
+            Self::Missing | Self::Forced | Self::ChangedResource => true,
+            Self::Existing => false,
+        }
     }
 
-    fn build_query(config: ConfigQuery) -> Result<Query> {
-        let config = Query::builder()
-            .default_field(Some("value".to_string()))
-            .default_one(false)
-            .default_selector("<web-resource>".to_string())
-            .build(config)?;
-        if config.one {
-            return Err(anyhow!("web-resource prequery does not support --one"));
+    pub fn reason(self) -> Option<&'static str> {
+        match self {
+            Self::Missing => None,
+            Self::Forced => Some("overwrite of existing files was forced"),
+            Self::ChangedResource => Some("URL has changed"),
+            Self::Existing => Some("file exists"),
         }
+    }
 
-        Ok(config)
+    fn print_reason(self) {
+        if let Some(msg) = self.reason() {
+            print!(" ({msg})");
+        }
+    }
+
+    pub fn print(self, name: &str, url: &str, path: &str) {
+        if self.download() {
+            print!("[{name}] Downloading {url} to {path}");
+            self.print_reason();
+            println!("...");
+        } else {
+            print!("[{name}] Downloading of {url} to {path} skipped");
+            self.print_reason();
+            println!();
+        }
     }
 }
 
-impl PreprocessorDefinition for WebResourceFactory {
-    const NAME: &'static str = "web-resource";
-
-    fn configure(
+impl WebResource {
+    pub(crate) fn new(
         name: String,
-        config: toml::Table,
-        query: ConfigQuery,
-    ) -> Result<BoxedPreprocessor> {
-        let config = Self::parse_config(config)?;
-        // index begins as None and is asynchronously populated later
-        let index = None;
-        let query = Self::build_query(query)?;
-        let instance = WebResource::new(name, config, index, query);
-        Ok(Box::new(Arc::new(instance)))
+        config: Config,
+        index: Option<Mutex<Index>>,
+        query: Query,
+    ) -> Self {
+        Self {
+            name,
+            index,
+            config,
+            query,
+        }
+    }
+
+    async fn populate_index(&mut self) -> Result<()> {
+        if let Some(location) = self.config.resolve_index_path().await {
+            // an index is in use
+            let location = location?;
+            let index = if fs::try_exists(&location).await.unwrap_or(false) {
+                // read the existing index
+                Index::read(location).await?
+            } else {
+                // generate an empty index
+                Index::new(location)
+            };
+
+            self.index = Some(Mutex::new(index));
+        } else {
+            // no index is in use
+            self.index = None;
+        }
+
+        Ok(())
+    }
+
+    async fn query(&self) -> Result<QueryData> {
+        self.query.query().await
+    }
+
+    async fn download(self: Arc<Self>, resource: Resource) -> Result<()> {
+        let name = self.name();
+        let Resource { url, path } = &resource;
+
+        let resolved_path = ARGS.resolve(path).with_context(|| {
+            let path_str = path.to_string_lossy();
+            format!("[{name}] cannot download to {path_str} because it is outside the project root")
+        })?;
+        let path_str = resolved_path.to_string_lossy();
+
+        let exists = fs::try_exists(&resolved_path).await.unwrap_or(false);
+        let state = if !exists {
+            ResourceState::Missing
+        } else if self.config.overwrite {
+            ResourceState::Forced
+        } else if let Some(index) = &self.index {
+            let index = index.lock().await;
+            if index.is_up_to_date(path, url) {
+                ResourceState::Existing
+            } else {
+                ResourceState::ChangedResource
+            }
+        } else {
+            ResourceState::Existing
+        };
+
+        state.print(name, url, &path_str);
+
+        if state.download() {
+            let result = self.do_download(&resolved_path, url).await;
+            match &result {
+                Ok(()) => {
+                    if let Some(index) = &self.index {
+                        let mut index = index.lock().await;
+                        index.update(resource.clone());
+                    }
+                    println!("[{name}] Downloading {url} to {path_str} finished");
+                }
+                Err(error) => {
+                    println!("[{name}] Downloading {url} to {path_str} failed: {error:?}");
+                }
+            }
+            result?;
+        }
+
+        Ok(())
+    }
+
+    async fn do_download(&self, resolved_path: &Path, url: &String) -> Result<()> {
+        if let Some(parent) = resolved_path.parent() {
+            fs::create_dir_all(parent).await?;
+        }
+        let mut response = reqwest::get(url).await?.error_for_status()?;
+        let mut file = fs::File::create(&resolved_path).await?;
+        while let Some(chunk) = response.chunk().await? {
+            file.write_all(&chunk).await?;
+        }
+        file.flush().await?;
+        Ok(())
+    }
+}
+
+#[async_trait]
+impl Preprocessor for Arc<WebResource> {
+    fn name(&self) -> &str {
+        &self.name
+    }
+
+    async fn run(&mut self) -> Result<()> {
+        Arc::get_mut(self)
+            .expect("web-resource ref count should be one before starting the processing")
+            .populate_index()
+            .await?;
+
+        let query_data = self.query().await?;
+
+        let mut set = JoinSet::new();
+        for (path, url) in query_data.resources {
+            set.spawn(Arc::clone(self).download(Resource { path, url }));
+        }
+
+        let mut success = true;
+        while let Some(result) = set.join_next().await {
+            let result = result?;
+            success &= result.is_ok();
+        }
+
+        if let Some(index) = &self.index {
+            let index = index.lock().await;
+            index.write().await?;
+        }
+
+        success
+            .then_some(())
+            .ok_or(anyhow!("at least one download failed"))
     }
 }
---
+++ src/preprocessors/web_resource/factory.rs
@@ -0,0 +1,52 @@
+use std::sync::Arc;
+
+use anyhow::{anyhow, Context, Result};
+
+use crate::config;
+use crate::preprocessor::{BoxedPreprocessor, PreprocessorDefinition};
+use crate::query::Query;
+
+use super::{Config, WebResource};
+
+/// The `web-resource` preprocessor factory
+#[derive(Debug, Clone, Copy)]
+pub struct WebResourceFactory;
+
+impl WebResourceFactory {
+    fn parse_config(config: toml::Table) -> Result<Config> {
+        let config = config
+            .try_into()
+            .context("invalid web-resource configuration")?;
+        Ok(config)
+    }
+
+    fn build_query(config: config::Query) -> Result<Query> {
+        let config = Query::builder()
+            .default_field(Some("value".to_string()))
+            .default_one(false)
+            .default_selector("<web-resource>".to_string())
+            .build(config)?;
+        if config.one {
+            return Err(anyhow!("web-resource prequery does not support --one"));
+        }
+
+        Ok(config)
+    }
+}
+
+impl PreprocessorDefinition for WebResourceFactory {
+    const NAME: &'static str = "web-resource";
+
+    fn configure(
+        name: String,
+        config: toml::Table,
+        query: config::Query,
+    ) -> Result<BoxedPreprocessor> {
+        let config = Self::parse_config(config)?;
+        // index begins as None and is asynchronously populated later
+        let index = None;
+        let query = Self::build_query(query)?;
+        let instance = WebResource::new(name, config, index, query);
+        Ok(Box::new(Arc::new(instance)))
+    }
+}
