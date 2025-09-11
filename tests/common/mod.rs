14942b372f7c852a7c723b6385ab8befd2151dab
Clemens Koza <clemens.koza@gmx.at>
2025-09-11 20:47:00 +0200

    use the common code for the dummy test case, making it Windows compatible

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 use clap::Parser;
 
 use mockall::predicate::eq;
--- tests/dummy_test.rs
+++ tests/dummy_test.rs
@@ -1,19 +1,13 @@
-use clap::Parser;
-
 use mockall::predicate::{always, eq};
-use prequery_preprocess::VecLog;
-use prequery_preprocess::args::CliArguments;
-use prequery_preprocess::entry::run;
-use prequery_preprocess::error::Result;
 use prequery_preprocess::log;
-use prequery_preprocess::manifest::PrequeryManifest;
-use prequery_preprocess::preprocessor::{
-    MockPreprocessor, MockPreprocessorDefinition, PreprocessorMap,
-};
+use prequery_preprocess::preprocessor::{MockPreprocessor, MockPreprocessorDefinition};
+use prequery_preprocess::query::Query;
 use prequery_preprocess::world::{MockWorld, World};
 
+mod common;
+
 #[tokio::test]
-async fn run_dummy() -> Result<()> {
+async fn run_dummy() {
     // dummy preprocessor that is used by the configuration
     let mut dummy = MockPreprocessorDefinition::<MockWorld>::new();
     dummy.expect_name().return_const("dummy");
@@ -41,42 +35,33 @@ async fn run_dummy() -> Result<()> {
     // must not be used to configure an instance
     dummy2.expect_configure().never();
 
-    // mock world that contains the two preprocessors and basic setup
-    let log = VecLog::new();
-    let mut world = MockWorld::new();
-    world.expect_preprocessors().return_const({
-        let mut preprocessors = PreprocessorMap::new();
-        preprocessors.register(dummy);
-        preprocessors.register(dummy2);
-        preprocessors
-    });
-    world
-        .expect_arguments()
-        .return_const(CliArguments::parse_from([
-            "prequery-preprocess",
-            "input.typ",
-        ]));
-    world.expect_log().return_const(log.clone());
-    world.expect_read_typst_toml().returning(|| {
-        PrequeryManifest::parse(
-            r#"
-            [package]
-            name = "test"
-            version = "0.0.1"
-            entrypoint = "main.typ"
+    common::PreprocessorTest::new(
+        |preprocessors| {
+            preprocessors.register(dummy);
+            preprocessors.register(dummy2);
+        },
+        &["prequery-preprocess", "input.typ"],
+        r#"
+        [package]
+        name = "test"
+        version = "0.0.1"
+        entrypoint = "main.typ"
 
-            [[tool.prequery.jobs]]
-            name = "test"
-            kind = "dummy"
-            "#,
-        )
-    });
-
-    // run the world
-    run(world).await?;
-
-    // assert correct logging
-    assert_eq!(log.get_lossy(), include_str!("dummy/run.txt"));
-
-    Ok(())
+        [[tool.prequery.jobs]]
+        name = "test"
+        kind = "dummy"
+        "#,
+        // unused
+        Query {
+            selector: Default::default(),
+            field: Default::default(),
+            one: Default::default(),
+            inputs: Default::default(),
+        },
+        b"",
+    )
+    .run()
+    .await
+    .expect_ok("dummy job should succeed")
+    .expect_log(include_str!("dummy/run.txt"));
 }
